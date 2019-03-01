2c1ae8d2df91ff28843e0737fb3b804f03c3a3b1
faraznazeer <farznaz98@gmail.com>
2019-03-01 21:52:46 +0530

    Second Commit

--- Mapper.py
+++ Mapper.py
@@ -3,8 +3,19 @@ from Database import Database
 class Mapper:
 
     def __init__(self):
-        database = Database()
+        self.database = Database()
+        self.data = None
+
+    '''def fetch_ICD_data(self):
+
+        self.database.connect('localhost', 'root', 'zaraf', 'ICD')
+        query = 'SELECT code,diagnosis FROM ICD_data'
+        self.data = self.database.fetch_data()
+    '''
+    def search():
+        pass
 
     def map(self, diagosis):
-        pass
+        return self.data
+
 
--- Tester.py
+++ Tester.py
@@ -1,33 +1,63 @@
 from Database import Database
 from Preprocessor import Preprocessor
+from Mapper import Mapper
+from collections import defaultdict
 
+dictionary1 = defaultdict(list)
+dictionary2 = defaultdict(list)
 database_object = Database()
 data_object = Preprocessor()
-value = []
+mapper = Mapper()
 
-def insert_data():
+value = []
+query = " SELECT * FROM ICD_data WHERE diagnosis LIKE %s AND diagnosis LIKE %s"
+query1 = "UPDATE ICD_data SET diagnosis = %s WHERE code = %s "
+query2 = 'SELECT code,diagnosis FROM ICD_data'
+
+def update_data():
     
     (icd, diagnosis) = data_object.get_full_data()
-    query = "UPDATE ICD_data SET diagnosis = %s WHERE code = %s "
    
     for i in range( len(icd) ): 
         value.append( ( ' '.join(diagnosis[i]) , icd[i] ) ) 
 
     database_object.insert_many( query, value  )
 
-
-
-def fetch_data():
-
-    query = " SELECT * FROM ICD_data WHERE diagnosis LIKE %s AND diagnosis LIKE %s"
+'''
+def fetch_data():   
     value = ('%anemia%', '%unspecified%')
     print ( x for x in database_object.fetch_data(query, value) )
 
+'''
 
+def map_data(diagnosis):
 
+    data = database_object.fetch_data(query2, None)
+    
+    for disease in diagnosis:
 
+        for item in data:
+            icds = list(item)
+
+            if( disease in icds[1].split(' ') ):
+                dictionary1[disease].append(icds[0])
+                dictionary2[disease].append(icds[1])
+
+        print(disease+"\n")
+        if ( disease.find(' ') == -1):
+            for i in range(len(dictionary1[disease])):
+                
+                if 'unspecified' in dictionary2[disease][i].split(' ') and len(dictionary2[disease][i].split(' ')) == 2:
+                    print (dictionary1[disease][i]+"\n")
+
+   # print(dictionary)   
 
 if __name__ == '__main__':
 
     database_object.connect('localhost', 'root', 'zaraf', 'ICD')
-    insert_data()
+    #fetch_data()
+
+    diagnosis1 = ['Anemia']
+    #diagnosis2 = ['']
+    map_data(diagnosis1 )
+    
